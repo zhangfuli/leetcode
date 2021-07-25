07ba9d5ac1e6a049593da62e2169679e5364b0b8
zhangfuli <1171067930@qq.com>
2021-07-25 17:51:50 +0800

    又是一个全排列

--- 笔试题/251场周赛/5825. 最大兼容性评分和.py
+++ 笔试题/251场周赛/5825. 最大兼容性评分和.py
@@ -1,31 +1,41 @@
 class Solution:
-    def maxCompatibilitySum(self, students, mentors):
-        res = 0
-        s = []
-        for i in range(len(students)):
-            if students[i] in mentors:
-                res += len(students[i])
-                mentors.remove(students[i])
-            else:
-                s.append(students[i])
-        students = s
 
-        hashmap = {}
-        for i in range(len(students)):
-            for j in range(len(mentors)):
-                if tuple(students[i]) not in hashmap:
-                    hashmap[tuple(students[i])] = []
-                score = 0
-                for z in range(len(students[i])):
-                    if students[i][z] == mentors[j][z]:
-                        score += 1
-                hashmap[tuple(students[i])].append(score)
-        print(hashmap)
-        while hashmap:
+    # 抽取计算评分功能，这样在回溯时可以把二维数组看作一维，更容易理解
+    def calculate(self, listOne, listTwo):
+        sum = 0
+        for i in range(len(listOne)):
+            if listOne[i] == listTwo[i]:
+                sum += 1
+        return sum
 
+    # 本质是一个数组不变，另一个穷尽其所有排列，尝试所有可能
+    def dfs(self, visited, index, sum):
+        '''
+        index即其中students的下标，变化为从0---n-1
+        mentors用于回溯穷举出全排列
+        '''
+        if index >= self.n:
+            self.max = max(self.max, sum)
+        # 对mentors进行回溯尝试求其全排列
+        for i in range(self.n):
+            # 避开已经访问过的
+            if visited[i] == 0:
+                # 递归
+                visited[i] = 1
+                addNum = self.calculate(self.students[index], self.mentors[i])
+                sum += addNum
+                self.dfs(visited, index + 1, sum)
+                sum -= addNum
+                visited[i] = 0
+                # 回溯
 
-
-solution = Solution()
-solution.maxCompatibilitySum(
-    [[0, 0, 1, 1, 1, 0, 1], [0, 1, 1, 0, 0, 0, 0], [0, 0, 1, 1, 1, 1, 1], [0, 1, 0, 0, 1, 0, 1], [1, 0, 1, 1, 1, 1, 1]],
-    [[0, 1, 1, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 1], [0, 1, 0, 1, 0, 0, 1], [1, 0, 0, 0, 1, 0, 1], [1, 1, 1, 1, 1, 0, 0]])
+    def maxCompatibilitySum(self, students, mentors) -> int:
+        # 置为成员变量减少参数传递
+        self.students = students
+        self.mentors = mentors
+        self.n, self.m = len(students), len(students[0])
+        self.max = 0
+        # 标记是否被访问过
+        visited = [0 for _ in range(self.n)]
+        self.dfs(visited, 0, 0)
+        return self.max
