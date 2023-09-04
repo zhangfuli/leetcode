96588162506f3cf63e255c4aa1b9c0f35d0cc0a0
zhangfuli <1171067930@qq.com>
2023-09-04 15:27:51 +0800

    Update 116. 填充每个节点的下一个右侧节点指针.py

--- 手把手刷数据结构/二叉树/116. 填充每个节点的下一个右侧节点指针.py
+++ 手把手刷数据结构/二叉树/116. 填充每个节点的下一个右侧节点指针.py
@@ -11,15 +11,17 @@ class Node:
 
 class Solution:
     def connect(self, root):
-        if root == None or root.left == None or root.right == None:
+        if root is None or root.left is None or root.right is None:
             return root
-        self.connectTwo(root.left, root.right)
 
-    def connectTwo(self, node1, node2):
-        if node1 == None or node2 == None:
+        self.traverse(root.left, root.right)
+        return root
+
+    def traverse(self, node1, node2):
+        if node1 is None or node2 is None:
             return None
 
         node1.next = node2
-        self.connectTwo(node1.left, node1.right)
-        self.connectTwo(node2.left, node2.right)
-        self.connectTwo(node1.right, node2.left)
+        self.traverse(node1.left, node1.right)
+        self.traverse(node2.left, node2.right)
+        self.traverse(node1.right, node2.left)
